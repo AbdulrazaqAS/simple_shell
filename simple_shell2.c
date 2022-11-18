292b9f55136bdc61ec5217bae12b77f16be76157
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-11-18 07:42:15 -0800

    shell

--- simple_shell2.c
+++ simple_shell2.c
@@ -7,20 +7,19 @@
 void execute(char *);
 
 char *av[] = {NULL, NULL};
-char *env[] = {"USER=abdulrazaq", "LOGNAME=abdulrazaq",
-	"HOME=/simple_shell", NULL};
+char *env[] = {"USER=abdulrazaq", "LOGNAME=abdulrazaq", "HOME=/simple_shell", NULL};
 pid_t child_pid;
-imt wstatus;
+int wstatus;
 char *file;
 
 /**
- * main - entry
+ * main - entry point
  * @argc: arg count
  * @argv: arg vector
  *
  * Return: 0 or -1
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	size_t len = 0;
 	ssize_t count;
@@ -29,14 +28,15 @@ int main(int argc, char *argv[])
 	file = argv[0];
 	argc++;
 	argc--;
-
+	
 	do {
-		printf("abdulrazaq$ ");
+		printf("#abdulrazaq$ ");
 		count = getline(&buf, &len, stdin);
 
-		buf[count - 1] = '\n';
+		buf[count - 1] = '\0';
 		if (count != -1)
 			execute(buf);
+
 	} while (count != -1);
 
 	putchar('\n');
@@ -47,27 +47,28 @@ int main(int argc, char *argv[])
 }
 
 /**
- * execute - exec a given command
- * @buf: command to exec
+ * execute - exec the required command
+ * @buf: cmd name
  */
 void execute(char *buf)
 {
 	child_pid = fork();
 	if (child_pid == -1)
-	{
-		perror(file);
-		exit(EXIT_FAILURE);
-	}
-
-	if (child_pid == 0)
-	{
-		av[0] - buf;
-		execve(av[0], av, env);
-		perror(file);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
+       	{
+               	perror(file);
+               	exit(EXIT_FAILURE);
+       	}
+       	if (child_pid == 0)
+       	{
+		av[0] = buf;
+               	execve(av[0], av, env);
+               	perror(file);
+               	exit(EXIT_FAILURE);
+       	}
+       	else
+       	{
 		wait(&wstatus);
-	}
+       	}
+
+	return;
 }
